b80a996a1e5c4fa90e0948eba457ab01c1f27299
Rostan <rodrigorostan@outlook.com>
2025-04-29 23:02:33 -0300

    feat create/delete bucket scripts

---
+++ src/buckets-config/create_buckets.sh
@@ -0,0 +1,2 @@
+aws s3api create-bucket --bucket $AWS_RAW_BUCKET --region $AWS_REGION
+aws s3api create-bucket --bucket $AWS_CURATED_BUCKET --region $AWS_REGION
---
+++ src/buckets-config/delete_buckets.sh
@@ -0,0 +1,2 @@
+aws s3api delete-bucket --bucket $AWS_RAW_BUCKET --region $AWS_REGION
+aws s3api delete-bucket --bucket $AWS_CURATED_BUCKET --region $AWS_REGION
